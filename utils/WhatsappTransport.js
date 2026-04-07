d18c12dbae809ebf3c5f407bbd73045c33bf78f2
uberbl4tt <burboncat45@gmail.com>
2026-04-07 08:55:24 +0700

    refactor(transport): reformat whatsapp transport message structure

--- utils/WhatsappTransport.js
+++ utils/WhatsappTransport.js
@@ -10,12 +10,19 @@ class WhatsappTransport extends TransportBase {
   async log(info, callback) {
     try {
       if (!this.client) return callback();
-      const { message, userId } = info;
+      const { message, messageId } = info;
 
       await this.client.sendMessage(
         this.chatId,
-        `🚨 _Bot Error_\n*Message*: \`${message}\` \n*User*: ${userId || "-"}`,
+        `🚨 _Bot Error_\n*Message*: \`${message}\` \n*User*: ${messageId.from || "-"}`,
       );
+
+      if (messageId) {
+        await this.client.sendMessage(
+          this.chatId,
+          await messageId.getContact()
+        )
+      }
       callback();
     } catch (err) {
       console.error(`Error in WhatsappTransport: ${err}`);
